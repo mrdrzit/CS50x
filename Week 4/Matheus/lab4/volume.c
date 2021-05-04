f0b10f1d3acd719806bf2f3b7fe38a956980f8ce
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-05-04 09:39:18 -0300

    Last commit on the lab4

--- Week 4/Matheus/lab4/volume.c
+++ Week 4/Matheus/lab4/volume.c
@@ -6,6 +6,9 @@
 
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
+typedef uint8_t BYTE;
+typedef int16_t SAMPLE;
+
 
 int main(int argc, char *argv[])
 {
@@ -17,14 +20,14 @@ int main(int argc, char *argv[])
     }
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
+    FILE *input = fopen("C:\\Users\\uzuna\\Documents\\Harvard course Computer Science\\Weeks\\Week 4\\Matheus\\lab4/\\input.wav", "rb");
     if (input == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
 
-    FILE *output = fopen(argv[2], "w");
+    FILE *output = fopen("C:\\Users\\uzuna\\Documents\\Harvard course Computer Science\\Weeks\\Week 4\\Matheus\\lab4/\\output.wav", "wb");
     if (output == NULL)
     {
         printf("Could not open file.\n");
@@ -34,9 +37,22 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    FILE *wave = fopen("input.wave","r");
+    BYTE bytes[44] = {};
+    SAMPLE samples[88200] = {};
+
+    fread(&bytes, sizeof(BYTE), 44, input);
+    fwrite(&bytes,sizeof(BYTE), 44, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int idx = 0;
+    int16_t buffer;
+    while(idx < sizeof(samples))
+    {
+        fread(&buffer, sizeof(SAMPLE), 1, input);
+        buffer = buffer * factor;
+        fwrite(&buffer, sizeof(SAMPLE), 1, output);
+        idx++;
+    }
 
     // Close files
     fclose(input);
