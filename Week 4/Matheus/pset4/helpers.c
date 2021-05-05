5f9d4a458254ee8d8139abae53fbda3d6750d7c2
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-05-04 21:27:51 -0300

    Implemented Grayscale

--- Week 4/Matheus/pset4/helpers.c
+++ Week 4/Matheus/pset4/helpers.c
@@ -3,6 +3,19 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int cols = width;
+    int rows = height;
+
+    for (int irow = 0; irow < rows; irow++)
+    {
+        for (int icol = 0; icol < cols; icol++)
+        {
+            int gray = round((image[irow][icol].rgbtBlue + image[irow][icol].rgbtGreen + image[irow][icol].rgbtRed) / 3.0);
+            image[irow][icol].rgbtBlue = gray;
+            image[irow][icol].rgbtRed = gray;
+            image[irow][icol].rgbtGreen = gray;
+        }
+    }
     return;
 }
 
