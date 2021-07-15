3ec2be8580d55baaca40d9fbbe75702dae387b21
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-07-15 10:20:02 -0300

    Sou burro demais kkkk mds.. escrevi "elseif" ao invés de "else if"

--- Week 5/Matheus/class_files/binary_tree.c
+++ Week 5/Matheus/class_files/binary_tree.c
@@ -14,19 +14,19 @@ node;
 //Implement a function to search a tree and return true of false
 bool search(node *tree, int number)
 {
-    // if (tree == NULL)
-    // {
-    //     return false
-    // }elseif (number < tree->number)
-    //     {
-    //         return search(tree->left, number);
-    //     }
-    //     elseif (number > tree->number)
-    //     {
-    //         return search(tree->left, number);
-    //     }
-    //     else
-    //     {
-    //         return true;
-    //     }
-}
+    if (tree == NULL)
+    {
+        return false;
+    }
+    else if (number < tree->number)
+    {
+        return search(tree->left, number);
+    }
+    else if (number > tree->number)
+    {
+        return search(tree->left, number);
+    }
+    else
+    {
+        return true;
+    }
