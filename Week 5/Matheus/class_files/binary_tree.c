1c4d0a1f467955cded0c7ec5d461fe881db730a2
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-07-14 21:57:46 -0300

    Binary tree

---
+++ Week 5/Matheus/class_files/binary_tree.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node
+{
+    int number;
+    struct node *left;
+    struct node *right;
+}
+node;  
+
+//Implement a function to search a tree and return true of false
+bool search(node *tree, int number)
+{
+    // if (tree == NULL)
+    // {
+    //     return false
+    // }elseif (number < tree->number)
+    //     {
+    //         return search(tree->left, number);
+    //     }
+    //     elseif (number > tree->number)
+    //     {
+    //         return search(tree->left, number);
+    //     }
+    //     else
+    //     {
+    //         return true;
+    //     }
+}
